7886e8331953c24228aa861c5ee6a478e913a63f
AceChuse <1825758650@qq.com>
2021-12-29 23:28:19 +0800

    Add files via upload

--- Omni/omni_fmeta.py
+++ Omni/omni_fmeta.py
@@ -6,21 +6,27 @@ shot = 1
 nc = 5
 model_name = 'fuzzymeta4'
 lossf = 'cross_entropy'
+inner_num = 1
 
 query = shot
 ms = 32 if nc == 5 else 16
-
+''''''
 if os.system('python3 omniglot_e.py --train=True --picshow=False --metatrain_itr=60000'
-             ' --meta_batch_size='+str(ms)+' --num_classes='+str(nc)+' --kshot='+str(shot)+
-             ' --lossf='+lossf+' --kquery='+str(query)+' --inner_num=1 --model_name='+model_name):
+             ' --meta_batch_size='+str(ms)+' --num_classes='+str(nc)+' --kshot='+str(shot)+''
+             ' --lossf='+lossf+' --kquery='+str(query)+' --inner_num='+str(inner_num)+' --model_name='+model_name):
     raise ValueError('Here1!')
+
+# if os.system('python3 omniglot_e.py --resume=True --train=True --picshow=False --metatrain_itr=60000'
+#              ' --resume_path=_i4300.pkl --meta_batch_size='+str(ms)+' --num_classes='+str(nc)+''
+#              ' --lossf='+lossf+' --kshot='+str(shot)+' --kquery='+str(query)+' --inner_num='+str(inner_num)+' --model_name='+model_name):
+#     raise ValueError('Here1!')
 
 if os.system('python3 omniglot_e.py --train=False --picshow=False --test_or_val=False --len_test=60'
              ' --lossf='+lossf+' --test_interval=1000 --num_classes='+str(nc)+' --kshot='+str(shot)+''
-             ' --kquery='+str(query)+' --inner_num=1 --model_name='+model_name):
+             ' --kquery='+str(query)+' --inner_num='+str(inner_num)+' --model_name='+model_name):
     raise ValueError('Here2!')
 
 if os.system('python3 omniglot_e.py --train=False --picshow=False --test_or_val=True'
-             ' --num_classes='+str(nc)+' --kshot='+str(shot)+' --kquery='+str(query)+' --inner_num=1'
+             ' --num_classes='+str(nc)+' --kshot='+str(shot)+' --kquery='+str(query)+' --inner_num='+str(inner_num)+''
              ' --lossf='+lossf+' --model_name='+model_name):
     raise ValueError('Here3!')
